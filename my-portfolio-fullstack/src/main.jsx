d58c9cc68ce997f2339cb531f462c456e860412d
CandelaReynoso <mariacandelamusica@gmail.com>
2023-05-09 21:59:52 -0300

    modified main.jsx

--- my-portfolio-fullstack/src/main.jsx
+++ my-portfolio-fullstack/src/main.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter de react-router-dom
+import { BrowserRouter, Route, Switch } from 'react-router-dom'; // Importa BrowserRouter, Route y Switch de react-router-dom
 import App from './App.jsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
+// Importa los componentes que serán renderizados en cada ruta
+import Home from './components/Home.jsx';
+import Resume from './components/Resume.jsx';
+import Projects from './components/Projects.jsx';
+import Contact from './components/Contact.jsx';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter> 
-      <App />
+    <BrowserRouter>
+      <App>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/resume" component={Resume} />
+          <Route exact path="/projects" component={Projects} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </App>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
